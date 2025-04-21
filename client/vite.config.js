ec36cb4e5215470c154e89de4e5db7d55ebad7bf
sophieshelley <sophiashelley17@gmail.com>
2025-04-21 01:49:35 -0600

    fix for render cors issue

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,12 +4,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 3000,
-    proxy: {
-      '/api': {
-        target: 'https://postgresusermanagement-1.onrender.com', 
-        changeOrigin: true,  
-        secure: false,  
-      },
-    },
+    allowedHosts: ['postgresusermanagement-1.onrender.com'], 
   },
 });
+
